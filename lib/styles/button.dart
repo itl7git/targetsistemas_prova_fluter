4580f0a90f7ac5248780ab043cfd667f2f6ede1e
Italo Leal <itlcoorporativo@gmail.com>
2023-11-06 02:31:00 -0300

    style: Adiciona um style para o button da login_screen

---
+++ lib/styles/button.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+final ButtonStyle buttonStyle = ElevatedButton.styleFrom(
+    minimumSize: const Size(44, 44),
+    backgroundColor: Colors.green,
+    elevation: 0,
+    shape: const RoundedRectangleBorder(
+        borderRadius: BorderRadius.all(Radius.circular(50))));
